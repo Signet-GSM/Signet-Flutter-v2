0517a4ea445b58ffc7c0e5089c10ed695ca3857b
parkyuhyeon <pyh1111802@naver.com>
2024-09-24 09:10:27 +0900

    :lipstick: :: update home_page

--- lib/views/home/home_page.dart
+++ lib/views/home/home_page.dart
@@ -1,11 +1,24 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:signet/components/info_components.dart';
 part '../home/home_scaffold.dart';
+part '../home/home_header.dart';
+part '../home/home_popular_card.dart';
+part '../home/home_add_gallery_card.dart';
+part '../home/home_popular_info.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const _Scaffold();
+    return const _Scaffold(
+      header: _Header(),
+      popularGalleryInfo: _PopularGalleryInfo(),
+      popularGalleryCard: _PopularGalleryCard(),
+      addGalleryCard: _PopularGalleryCard(),
+      popularBoardInfo: _PopularBoardInfo(),
+      popularBoardCard: _PopularGalleryCard(),
+    );
   }
 }
