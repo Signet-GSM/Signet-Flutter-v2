17e673e136a43def20071dbcaa67a08e5eae283d
parkyuhyeon <pyh1111802@naver.com>
2024-09-24 09:09:40 +0900

    :lipstick: :: update home scaffold

--- lib/views/home/home_scaffold.dart
+++ lib/views/home/home_scaffold.dart
@@ -1,10 +1,48 @@
 part of '../home/home_page.dart';
 
 class _Scaffold extends StatelessWidget {
-  const _Scaffold();
+  const _Scaffold({
+    required this.header,
+    required this.popularGalleryInfo,
+    required this.popularGalleryCard,
+    required this.addGalleryCard,
+    required this.popularBoardInfo,
+    required this.popularBoardCard,
+  });
+
+  final Widget header;
+  final Widget popularGalleryInfo;
+  final Widget popularGalleryCard;
+  final Widget addGalleryCard;
+  final Widget popularBoardInfo;
+  final Widget popularBoardCard;
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return Scaffold(
+      body: Stack(
+        children: [
+          header,
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 17),
+            child: Column(
+              children: [
+                const SizedBox(height: 150),
+                popularGalleryInfo,
+                const SizedBox(height: 18),
+                popularGalleryCard,
+                const SizedBox(height: 42),
+                addGalleryCard,
+                const SizedBox(height: 42),
+                popularBoardInfo,
+                const SizedBox(height: 18),
+                popularBoardCard,
+                const SizedBox(height: 34),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
