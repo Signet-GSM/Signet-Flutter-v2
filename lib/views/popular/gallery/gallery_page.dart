77c726045cd58a69bf48d61c53466a196b94d84b
parkyuhyeon <pyh1111802@naver.com>
2024-10-02 08:20:06 +0900

    :lipstick: :: create gallery page

---
+++ lib/views/popular/gallery/gallery_page.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+import 'package:signet/components/gallery_card_components.dart';
+import 'package:signet/components/header_components.dart';
+import 'package:signet/models/boards/gallery_models.dart';
+part './gallery_scaffold.dart';
+part './gallery_header.dart';
+part './gallery_popular_gallery.dart';
+
+class PopularGalleryPage extends StatelessWidget {
+  const PopularGalleryPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const _Scaffold(
+      header: _Header(),
+      popular_gallery: _PopularGallery(),
+    );
+  }
+}
