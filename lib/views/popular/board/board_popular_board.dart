a53f7946c5b227d2e4a0096c297cf441de50292f
parkyuhyeon <pyh1111802@naver.com>
2024-10-09 10:19:57 +0900

    :lipstick: :: create board popular board

---
+++ lib/views/popular/board/board_popular_board.dart
@@ -0,0 +1,22 @@
+part of './board_page.dart';
+
+class _PopularBoard extends StatelessWidget {
+  const _PopularBoard({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView.separated(
+      padding: const EdgeInsets.symmetric(horizontal: 18, vertical: 42),
+      shrinkWrap: true,
+      itemBuilder: (context, index) => BoardCardComponents(
+        board: BoardModels.init(),
+      ),
+      separatorBuilder: (context, index) => const Divider(
+        color: Color(0xffe8e8e8),
+        thickness: 1,
+        height: 20,
+      ),
+      itemCount: 100,
+    );
+  }
+}
