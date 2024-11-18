a262b45abde567c2c4854f7c577334626fc92dac
parkyuhyeon <pyh1111802@naver.com>
2024-11-18 20:06:36 +0900

    :lipstick: :: feature searchpage

---
+++ lib/views/search/search_page.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+part './search_scaffold.dart';
+
+class SearchPage extends StatelessWidget {
+  const SearchPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const _Scaffold();
+  }
+}
---
+++ lib/views/search/search_scaffold.dart
@@ -0,0 +1,10 @@
+part of './search_page.dart';
+
+class _Scaffold extends StatelessWidget {
+  const _Scaffold({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold();
+  }
+}
