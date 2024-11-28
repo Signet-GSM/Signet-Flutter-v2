c543bdb708dd4effbdf9ece14a86d73cddef3644
parkyuhyeon <pyh1111802@naver.com>
2024-11-28 20:02:00 +0900

    :lipstick: :: add search_page

--- lib/views/search/search_page.dart
+++ lib/views/search/search_page.dart
@@ -1,11 +1,14 @@
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 part './search_scaffold.dart';
+part './search_header.dart';
+part './search_textfield.dart';
 
 class SearchPage extends StatelessWidget {
   const SearchPage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const _Scaffold();
+    return const _Scaffold(header: _Header(),);
   }
 }
