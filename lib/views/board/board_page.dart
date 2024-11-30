13298b90fdedff0d0d3663a7958dd4f8c4661ab2
parkyuhyeon <pyh1111802@naver.com>
2024-12-01 00:12:25 +0900

    💄 :: create board_page

---
+++ lib/views/board/board_page.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+part './board_scaffold.dart';
+
+class BoardPage extends StatelessWidget {
+  const BoardPage ({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const _Scaffold();
+  }
+}
