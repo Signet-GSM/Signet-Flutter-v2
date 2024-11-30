4b969b9728e51d8efddff8e746183d585a1926a8
parkyuhyeon <pyh1111802@naver.com>
2024-12-01 00:13:47 +0900

    :lipstick: :: create board_scaffold

---
+++ lib/views/board/board_scaffold.dart
@@ -0,0 +1,13 @@
+part of './board_page.dart';
+
+class _Scaffold extends StatelessWidget {
+  const _Scaffold();
+
+  @override
+  Widget build(BuildContext context) {
+    return  Scaffold(
+      appBar: AppBar(),
+      body:  Column(children: [],),
+    );
+  }
+}
